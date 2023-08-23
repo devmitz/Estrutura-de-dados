c32599e667373533af7305d3b14d935e2de67e35
Felipe Schmitz <aryastarklinda@outlook.com>
2023-08-23 14:46:07 -0300

    feat:

--- Exercicio 3/Exercicio03.py
+++ Exercicio 3/Exercicio03.py
@@ -1,6 +1,16 @@
 def main():
-    ...
 
+    matriz = [[0 for _ in range(4)] for _ in range(4)]
 
+    print("#" * 49 ) 
+
+    for i in range(4):
+        print("#", end="\t")
+        for j in range(4):
+            matriz[i][j] = (i + 1) * (j + 1)
+            print(matriz[i][j], end="\t")
+        print("\t#")
+
+    print("#" * 49 )
 if __name__ == '__main__':
     main()
