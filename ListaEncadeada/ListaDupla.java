834619247a1aceabb6f3c62d2322dce70b5a0733
Felipe Schmitz <contato.felipeschmitz@gmail.com>
2023-09-20 09:41:32 -0300

    Listas

---
+++ ListaEncadeada/ListaDupla.java
@@ -0,0 +1,55 @@
+public class ListaDupla {
+    public static void main(String args[]){
+        CountryDoublyLinkedList countryList = new CountryDoublyLinkedList();
+
+        countryList.insertCountry("Brasil");
+        countryList.insertCountry("Estados Unidos");
+        countryList.insertCountry("França");
+        countryList.insertCountry("Japão");
+        countryList.insertCountry("Austrália");
+        countryList.insertCountry("Sri Lanka");
+
+        // Exibindo os países na ordem original
+        System.out.println("Países:");
+        countryList.displayCountries();
+    }
+
+    static class CountryNode {
+        String name;
+        CountryNode next;
+        CountryNode prev;
+    
+        public CountryNode(String name) {
+            this.name = name;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    
+    static class CountryDoublyLinkedList {
+        CountryNode head;
+    
+        public CountryDoublyLinkedList() {
+            this.head = null;
+        }
+            
+        public void insertCountry(String name) {
+            CountryNode newNode = new CountryNode(name);
+            if (head == null) {
+                head = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+        }
+    
+        public void displayCountries() {
+            CountryNode current = head;
+            while (current != null) {
+                System.out.println(current.name);
+                current = current.next;
+            }
+        }
+    }    
+}
---
+++ ListaEncadeada/ListaSimples.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class ListaSimples{
+    public static void main(String arg[]){
+        LinkedList<String> nomes = new LinkedList<String>();
+        nomes.add("Felipe");
+        nomes.add("Rodrigo");
+        nomes.add("OnlyFans");
+        nomes.add("Esquizo");
+        System.out.println(nomes);
+    }
+}
