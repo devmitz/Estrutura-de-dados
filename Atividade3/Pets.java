0b8fc086774f83d2679534afb0b147ed36afae63
Felipe Schmitz <contato.felipeschmitz@gmail.com>
2023-09-27 11:23:30 -0300

    atividade 3

--- Atividade3/Pets.java
+++ Atividade3/Pets.java
@@ -1,135 +1,229 @@
 package Atividade3;
 
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Pets {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> gatos = new ArrayList<String>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Gato> listaGatos = new ArrayList<>();
+        criarGatosDeTeste(listaGatos);
 
+        // for (int i = 0; i < 7; i++) {
+        //     System.out.println("Digite os dados do gato #" + (i + 1));
+        //     System.out.print("Código do gato (4 dígitos): ");
+        //     int codigo = scanner.nextInt();
+        //     scanner.nextLine(); // Consumir a quebra de linha
 
-        // System.out.print("Números de gatos atendidos: ");
-        // int gatosAtendidos = sc.nextInt();
+        //     System.out.print("Nome do gato: ");
+        //     String nome = scanner.nextLine();
 
+        //     System.out.print("Raça do gato: ");
+        //     String raca = scanner.nextLine();
 
-        // for(int i = 0; i < gatosAtendidos; i++){
-        //     System.out.printf("Código do gato [%d]: ", i);
-        //     int cod = sc.nextInt();
-        //     System.out.printf("Nome do gato [%d]: ", i);
-        //     String nomeGato = sc.nextLine();
-        //     System.out.printf("Raca do gato [%d]: ", i);
-        //     String raca = sc.nextLine();
-        //     System.out.printf("Idade do gato [%d]: ", i);
-        //     int idade = sc.nextInt();
-        //     System.out.printf("Doenca do gato [%d]: ", i);
-        //     String doenca = sc.nextLine();
-        //     System.out.printf("Valor do atendimento [%d]: ", i);
-        //     double preco = sc.nextDouble();
-        //     gatos.add(new Gato(cod, nomeGato, raca, idade, doenca, preco));
+        //     System.out.print("Idade do gato: ");
+        //     int idade = scanner.nextInt();
+        //     scanner.nextLine(); // Consumir a quebra de linha
+
+        //     System.out.print("Doença do gato: ");
+        //     String doenca = scanner.nextLine();
+
+        //     System.out.print("Valor do atendimento: R$");
+        //     double valorAtendimento = scanner.nextDouble();
+        //     scanner.nextLine(); // Consumir a quebra de linha
+
+        //     Gato gato = new Gato(codigo, nome, raca, idade, doenca, valorAtendimento);
+        //     listaGatos.add(gato);
         // }
-        // Gato gato = new Gato();
-        // gato.codigo = 0253;
-        // gato.nome = "Vincent";
-        // gato.raca = "Siames";
-        // gato.idade = 10;
-        // gato.doenca = "insufieciência renal";
-        // gato.preco = 1233.21;
 
-        // gatos.add(new Gato(0253, "Vicent", "Siames", 10, "insuficiencia renal", 213.32));
-        gatos.add("Código: 0253; nome: Vincent; raca: Siamês; idade: 10; doenca: insuficiência renal crônica; preco: R$367,89");
-        gatos.add("Código: 7832; nome: Samuel; raca: Persa; idade: 12; doenca: diabetes; preco: R$1790,00");
-        gatos.add("Código: 1235; nome: Clément; raca: Angorá; idade: 7; doenca: enteropatia; preco: R$5435,65");
-        gatos.add("Código: 0001; nome: joão; raca: Siamês; idade: 4; doenca: fiv; preco: R$1000,89");
-        gatos.add("Código: 1327; nome: roberto; raca: persa; idade: 13; doenca: felv; preco: R$783,35");
-        gatos.add("Código: 0432; nome: gulag; raca: Siamês; idade: 8; doenca: diabetes tipo 2; preco: R$314,12");
-        gatos.add("Código: 0312; nome: alfredo; raca: Angorá; idade: 2; doenca: gripe; preco: R$52,12");
+        while (true) {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Consultar gatos por nome");
+            System.out.println("2. Consultar gatos por código do gato");
+            System.out.println("3. Ordenar por ordem alfabética");
+            System.out.println("4. Apagar dados de um gato");
+            System.out.println("5. Localizar a ordem dos dados de um gato na listagem");
+            System.out.println("6. Levantar quantos gatos possuem uma idade específica");
+            System.out.println("7. Somar o valor total dos atendimentos");
+            System.out.println("8. Sair");
+            System.out.print("Opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Consumir a quebra de linha
 
-        System.out.printf("Percorrendo o ArrayList (usando o índice)\n");
-        int n = gatos.size();
-        for (int i = 0; i < n; i++) {
-            System.out.printf("Posição %d- %s\n", i, gatos.get(i));
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome do gato: ");
+                    String nomeConsulta = scanner.nextLine();
+                    consultarGatosPorNome(listaGatos, nomeConsulta);
+                    break;
+                case 2:
+                    System.out.print("Digite o código do gato: ");
+                    int codigoConsulta = scanner.nextInt();
+                    scanner.nextLine(); // Consumir a quebra de linha
+                    consultarGatosPorCodigo(listaGatos, codigoConsulta);
+                    break;
+                case 3:
+                    ordenarPorOrdemAlfabetica(listaGatos);
+                    listarGatos(listaGatos);
+                    break;
+                case 4:
+                    System.out.print("Digite o código do gato a ser apagado: ");
+                    int codigoApagar = scanner.nextInt();
+                    scanner.nextLine(); // Consumir a quebra de linha
+                    apagarGato(listaGatos, codigoApagar);
+                    break;
+                case 5:
+                    System.out.print("Digite o nome do gato a ser localizado: ");
+                    String nomeLocalizar = scanner.nextLine();
+                    localizarOrdemGato(listaGatos, nomeLocalizar);
+                    break;
+                case 6:
+                    System.out.print("Digite a idade específica: ");
+                    int idadeConsulta = scanner.nextInt();
+                    scanner.nextLine(); // Consumir a quebra de linha
+                    int quantidadeGatosIdade = contarGatosPorIdade(listaGatos, idadeConsulta);
+                    System.out.println("Quantidade de gatos com " + idadeConsulta + " anos de idade: " + quantidadeGatosIdade);
+                    break;
+                case 7:
+                    double valorTotalAtendimentos = somarValorTotalAtendimentos(listaGatos);
+                    System.out.println("Valor total dos atendimentos: R$" + valorTotalAtendimentos);
+                    break;
+                case 8:
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
-
-        System.out.println();
-        System.out.println();
-
-        System.out.print("Digite o codigo a ser procurado: ");
-        String codProc = sc.nextLine();
-        Pattern pattern = Pattern.compile(codProc, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(gatos.get(1));
-        
-
-
-        sc.close();
     }
 
-    // public static class Gato {
-    //     private String nome;
-    //     private int codigo;
-    //     private int idade;
-    //     private double preco;
-    //     private String doenca;
-    //     private String raca;       
+    public static void criarGatosDeTeste(ArrayList<Gato> listaGatos) {
+        // Criar 7 gatos de teste
+        listaGatos.add(new Gato(1234, "Vincent", "Angorá", 5, "Insuficiência Renal Crônica", 367.89));
+        listaGatos.add(new Gato(5678, "Samuel", "Siamês", 3, "Diabetes", 1790.00));
+        listaGatos.add(new Gato(9012, "Clément", "Persa", 4, "Enteropatia", 5435.65));
+        listaGatos.add(new Gato(3456, "Amélie", "SRD", 2, "Fiv Felv", 800.50));
+        listaGatos.add(new Gato(7890, "François", "Angorá", 6, "Insuficiência Renal Crônica", 420.00));
+        listaGatos.add(new Gato(2345, "Sophie", "Siamês", 7, "Diabetes", 1200.75));
+        listaGatos.add(new Gato(6789, "Charlotte", "Persa", 5, "Enteropatia", 2200.25));
+    }
 
-    //     public Gato(int codigo, String nome, String raca, int idade, String doenca, double preco) {
-    //         this.codigo = codigo;
-    //         this.nome = nome;
-    //         this.raca = raca;
-    //         this.idade = idade;
-    //         this.doenca = doenca;
-    //         this.preco = preco;
-    //     }
+    public static void consultarGatosPorNome(ArrayList<Gato> listaGatos, String nome) {
+        for (Gato gato : listaGatos) {
+            if (gato.getNome().equalsIgnoreCase(nome)) {
+                System.out.println(gato);
+            }
+        }
+    }
 
-    //     public String getNome() {
-    //         return nome;
-    //     }
+    public static void consultarGatosPorCodigo(ArrayList<Gato> listaGatos, int codigo) {
+        for (Gato gato : listaGatos) {
+            if (gato.getCodigo() == codigo) {
+                System.out.println(gato);
+            }
+        }
+    }
 
-    //     public void setNome(String nome) {
-    //         this.nome = nome;
-    //     }
+    public static void ordenarPorOrdemAlfabetica(ArrayList<Gato> listaGatos) {
+        Collections.sort(listaGatos);
+    }
 
-    //     public int getCodigo() {
-    //         return codigo;
-    //     }
+    public static void listarGatos(ArrayList<Gato> listaGatos) {
+        for (int i = 0; i < listaGatos.size(); i++) {
+            System.out.println("Gato #" + (i + 1) + ": " + listaGatos.get(i));
+        }
+    }
 
-    //     public void setCodigo(int codigo) {
-    //         this.codigo = codigo;
-    //     }
+    public static void apagarGato(ArrayList<Gato> listaGatos, int codigo) {
+        for (int i = 0; i < listaGatos.size(); i++) {
+            if (listaGatos.get(i).getCodigo() == codigo) {
+                listaGatos.remove(i);
+                System.out.println("Gato removido com sucesso.");
+                return;
+            }
+        }
+        System.out.println("Gato não encontrado.");
+    }
 
-    //     public int getIdade() {
-    //         return idade;
-    //     }
+    public static void localizarOrdemGato(ArrayList<Gato> listaGatos, String nome) {
+        for (int i = 0; i < listaGatos.size(); i++) {
+            if (listaGatos.get(i).getNome().equalsIgnoreCase(nome)) {
+                System.out.println(nome + " é o " + (i + 1) + "º gato na listagem.");
+                return;
+            }
+        }
+        System.out.println("Gato não encontrado.");
+    }
 
-    //     public void setIdade(int idade) {
-    //         this.idade = idade;
-    //     }
+    public static int contarGatosPorIdade(ArrayList<Gato> listaGatos, int idade) {
+        int quantidade = 0;
+        for (Gato gato : listaGatos) {
+            if (gato.getIdade() == idade) {
+                quantidade++;
+            }
+        }
+        return quantidade;
+    }
 
-    //     public double getPreco() {
-    //         return preco;
-    //     }
+    public static double somarValorTotalAtendimentos(ArrayList<Gato> listaGatos) {
+        double total = 0;
+        for (Gato gato : listaGatos) {
+            total += gato.getValorAtendimento();
+        }
+        return total;
+    }
+}
 
-    //     public void setPreco(double preco) {
-    //         this.preco = preco;
-    //     }
+class Gato implements Comparable<Gato> {
+    private int codigo;
+    private String nome;
+    private String raca;
+    private int idade;
+    private String doenca;
+    private double valorAtendimento;
 
-    //     public String getDoenca() {
-    //         return doenca;
-    //     }
+    public Gato(int codigo, String nome, String raca, int idade, String doenca, double valorAtendimento) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.doenca = doenca;
+        this.valorAtendimento = valorAtendimento;
+    }
 
-    //     public void setDoenca(String doenca) {
-    //         this.doenca = doenca;
-    //     }
+    public int getCodigo() {
+        return codigo;
+    }
 
-    //     public String getRaca() {
-    //         return raca;
-    //     }
+    public String getNome() {
+        return nome;
+    }
 
-    //     public void setRaca(String raca) {
-    //         this.raca = raca;
-    //     }
-        
-    // }
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getDoenca() {
+        return doenca;
+    }
+
+    public double getValorAtendimento() {
+        return valorAtendimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo + ", Nome: " + nome + ", Raça: " + raca + ", Idade: " + idade + " anos, Doença: " + doenca + ", Valor do Atendimento: R$" + valorAtendimento;
+    }
+
+    @Override
+    public int compareTo(Gato outroGato) {
+        return this.getNome().compareTo(outroGato.getNome());
+    }
 }
