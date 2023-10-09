bb479fb630a4ccb28952c81ab6e63a6012a9ebd7
Felipe Schmitz <contato.felipeschmitz@gmail.com>
2023-10-09 09:38:17 -0300

    comentarios adicionais e função pop()

--- Aula7/Pilha.java
+++ Aula7/Pilha.java
@@ -1,25 +1,57 @@
 package Aula7;
 import Aula7.No;
 
-public class Pilha<T>{
-    No topo;
-    int tamanho;
-    
-    public Pilha(){
-        tamanho = 0;
-        topo = null;
+// Classe genérica chamada "Pilha" que armazena elementos do tipo T em uma pilha.
+public class Pilha<T> {
+
+    // Variável que mantém uma referência para o topo da pilha.
+    private No topo;
+    // Variável para rastrear o tamanho da pilha.
+    private int tamanho;
+
+    // Getters e Setters
+    public No getTopo() {
+        return topo;
     }
 
-    public boolean isEmpty(){
-        return topo == null;
+    public void setTopo(No topo) {
+        this.topo = topo;
     }
 
-    public void push(T info){
-        No no = new No();
-        no.setElemento(info);
-        no.setProximo(topo);
-        topo = no;
-        tamanho++;
+    public int getTamanho() {
+        return tamanho;
     }
 
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    // Construtor da pilha.
+    public Pilha() {
+        tamanho = 0; // Inicializa o tamanho como zero.
+        topo = null; // Inicializa o topo como nulo, indicando uma pilha vazia.
+    }
+
+    // Método para verificar se a pilha está vazia.
+    public boolean isEmpty() {
+        return topo == null; // A pilha está vazia se o topo for nulo.
+    }
+
+    // Método para adicionar um elemento à pilha (push).
+    public void push(T info) {
+        No no = new No(); // Cria um novo nó.
+        no.setElemento(info); // Define o elemento do nó com a informação passada.
+        no.setProximo(topo); // Define o próximo nó como o antigo topo.
+        topo = no; // Atualiza o topo para o novo nó.
+        tamanho++; // Aumenta o tamanho da pilha.
+    }
+
+    public Object pop(){
+        if( isEmpty() )
+            return null;
+        String info = topo.getElemento().toString();
+        topo = topo.getProximo();
+        tamanho--;
+        return info;
+    }
 }
--- Aula7/PilhaDinamica.java
+++ Aula7/PilhaDinamica.java
@@ -1,4 +1,6 @@
 package Aula7;
+import Aula7.No;
+import Aula7.Pilha;
 
 public class PilhaDinamica {
     public static void main(String[] args) {
